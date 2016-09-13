f4c8b3507f6c4a36a3912b11c1c1789a4d803126
DrScKim <smaslayer1@gmail.com>
2016-09-13 16:40:35 +0900

    Update srcnn_gen_filter.py

--- srcnn_gen_filter.py
+++ srcnn_gen_filter.py
@@ -1,5 +1,5 @@
 
-# this may help you test SRCNN without building 'matcaffe' you have to use SRCNN with your trained model of SRCNN but you don't want to build 'matcaffe'
+# this may help you test SRCNN without building 'matcaffe'
 # this python script can generate matrix compatible with SRCNN test matlab code
 # {your_download_path}/srcnn_gen_filter.py -caffemodel {the location of caffemodel you trained} -mat {the location of srcnn_mat.prototxt}
 # after you call this script, you can find 'filter.mat' file in the same {location of this project} 
